bf56fe93e3e56e7079d62b8a87cbe922223f3ecb
Lunazic <leon.lowitzki@innoki.de>
2016-12-06 01:38:41 +0100

    fix

--- ui.R
+++ ui.R
@@ -1,11 +1,6 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
+
+tags$h1(Playground)
 
 shinyUI(pageWithSidebar(
   
